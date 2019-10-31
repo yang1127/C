deb77b9945a9bb2653de303b590b0fbf08042f5b
yang1127 <1536520643@qq.com>
2019-10-31 19:37:55 +0800

    test

---
+++ C05/C05/C05.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+
+#include <stdio.h>
+
+int main()
+{
+	int i = 0, a = 0, b = 2, c = 3, d = 4; 
+	/*i = a++ && ++b && d++; */
+	i = a++||++b||d++;
+
+	printf("a = %d\nb = %d\nc = %d\nd = %d\n", a, b, c, d);
+	system("pause");
+	return 0; 
+} 
