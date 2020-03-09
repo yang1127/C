1831a1b8219eba19aad8ad46c51e9304bb71750c
yang1127 <1536520643@qq.com>
2020-03-09 22:51:54 +0800

    练习

--- C06指针/C06指针/test.c
+++ C06指针/C06指针/test.c
@@ -325,3 +325,68 @@
 //	 system("pause");
 //	 return 0;
 // }
+
+
+//4
+//#include <stdio.h>
+//int main(int argc, char * argv[])
+//{
+//	//int a[3][2] = { (0, 1), (2, 3), (4, 5) };  //1
+//	int a[3][2] = { 0, 1, 2, 3, 4, 5 }; //0
+//	int *p;
+//	p = a[0];
+//	printf("%d", p[0]); //1
+//}
+
+
+////5
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]); //FFFFFFFC,-4
+//	system("pause");
+//	return 0;
+//}
+
+//
+////6
+//
+//int main()
+//{
+//	int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int *ptr1 = (int *)(&aa + 1);
+//	int *ptr2 = (int *)(*(aa + 1));
+//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));//10.5
+//	system("pause");
+//	return 0;
+//}
+//
+//
+////7
+//#include <stdio.h>
+//int main()
+//{
+//	char *a[] = { "work","at","alibaba" };
+//	char**pa = a;
+//	pa++;
+//	printf("%s\n", *pa);// at
+//	system("pause");
+//	return 0;
+//}
+//
+//
+////8
+int main()
+{
+	char *c[] = { "ENTER","NEW","POINT","FIRST" };
+	char**cp[] = { c + 3,c + 2,c + 1,c };
+	char***cpp = cp;
+	printf("%s\n", **++cpp); //POINT
+	printf("%s\n", *--*++cpp + 3); //ER
+	printf("%s\n", *cpp[-2] + 3); //ST
+	printf("%s\n", cpp[-1][-1] + 1); //EW
+	system("pause");
+	return 0;
+}
--- C09/C09/test.c
+++ C09/C09/test.c
@@ -1,32 +1,36 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
+//strlen
 //int main()
 //{
 //	const char* str1 = "abcdef";
 //	printf("%d ", strlen(str1));//6 //strlen 遇到\0 停止
-//	printf("%d ", sizeof(str1));//8
+//	printf("%d ", sizeof(str1));// 4 / 8  char*
 //	printf("\n");
 //
 //	const char str2[] = "hello";
 //	printf("%d ", strlen(str2));//5
-//	printf("%d ", sizeof(str2));//6
+//	printf("%d ", sizeof(str2));//6 数组的大小 h e l l o \0
 //	printf("\n");
 //
 //	const char str3[] = "abcde\0fg\0";
 //	printf("%d ", strlen(str3));//5 a b c d e
-//	printf("%d ", sizeof(str3));//10 a b c d e \ 0 f g \0
+//	printf("%d ", sizeof(str3));//10 a b c d e \0 f g \0 \0
 //	printf("\n");
 //
 //	const char str4[] = "abcde\\0fg\\\0";
 //	printf("%d ", strlen(str4));//10 a b c d e \\ 0 f g \\ 
-//	printf("%d ", sizeof(str4));//12 a b c d e \\ 0 f g \\ \0
+//	printf("%d ", sizeof(str4));//12 a b c d e \\ 0 f g \\ \0 \0
 //	printf("\n");
 //
 //	const char str5[] = "abcde\\0fg\t\n\r";
 //	printf("%d ", strlen(str5));//12 a b c d e \\ 0 f g \t \n \r
-//	printf("%d ", sizeof(str5));//13 a b c d e \\ 0 f g \t \n \r
+//	printf("%d ", sizeof(str5));//13 a b c d e \\ 0 f g \t \n \r \0
 //	printf("\n");
 //
 //	system("pause");
@@ -37,15 +41,74 @@
 //size_t strlen(const char* str)
 //{
 //	int count = 0;
-//	while (*str != '\0') //*str
+//	while (*str != '\0') //遍历
 //	{
 //		count++;
 //		str++;
 //	}
 //	return count;
-//}////不能创建临时变量计数器
-//size_t strlen(const char* str)//{//	if (*str == '\0')
+//}
+//
+////不能创建临时变量计数器
+//size_t strlen(const char* str)
+//{
+//	if (*str == '\0')
 //		return 0;
 //	else
-//		return 1 + strlen(str + 1);//}//size_t strlen(char* str)//{//	char* p = str;//	while (*p != '\0')//	{//		p++;//	}//	return p - str;//}int main(){
-	const char* s1 = "abcdef";	printf("%zd ", strlen(s1));//6	system("pause");	return 0;}
+//		return 1 + strlen(str + 1); //递归
+//}
+//
+////指针—指针方式
+//size_t strlen(char* str)
+//{
+//	char* p = str;
+//	while (*p != '\0')
+//	{
+//		p++;
+//	}
+//	return p - str;
+//}
+//
+//int main()
+//{
+//	const char* s1 = "abcdef";
+//	printf("%d ", strlen(s1));//6
+//
+//	system("pause");
+//	return 0;
+//}
+
+//strcpy 
+//int main()
+//{
+//	char dst[20];
+//	strcpy(dst, "hello xiaoyang");
+//
+//	printf("%s\n", dst); // hello xiaoyang
+//	system("pause");
+//	return 0;
+//}
+
+//
+//char* my_strcpy(char* dst, const char* src)
+//{
+//	assert(src && dst); //判断是否越界
+//
+//	char* ret = dst;
+//	while (*dst++ = *src++);
+//
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char dst[20];
+//	my_strcpy(dst, "hello xiaoyang");
+//
+//	printf("%s\n", dst); // hello xiaoyang
+//
+//	system("pause");
+//	return 0;
+//}
+
+//strcat
